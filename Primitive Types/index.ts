81b3db72061a47dcbbf7ac453825193db4d7de7e
Ing-yeo_Developer <sjw7324@naver.com>
2022-04-11 17:06:18 +0900

    modify

---
+++ Primitive Types/index.ts
@@ -0,0 +1,19 @@
+let 이름: string = "seo";
+
+// 이름 = 123;                                 --- 에러
+let 나이: number = 50;
+let 결혼: boolean = false;
+
+let nullType: null = null;
+let undefinedType: undefined = undefined;
+
+let 회원들: string[] = ["kim", "park"];
+// 회원들 = [0, "park"]                        --- 에러
+let 여러가지: (string | number)[] = [1, "여러가지"];
+let 과일: { 붉은색: string; 노란색: string } = {
+  붉은색: "사과",
+  노란색: "바나나",
+};
+
+// ----------------------------------------------------------
+let 직업 = "백수";
